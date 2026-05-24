7ed51b63588122d8f16a903ddd8370e9150e550b
António Conselheiro <anarcanudos@proton.me>
2026-05-24 20:46:04 -0300

    jubileus

--- src/app/pages/timelines-page/timelines-page.ts
+++ src/app/pages/timelines-page/timelines-page.ts
@@ -305,9 +305,133 @@ export class TimelinesPage {
         source: [
           ['JUB', 4, 33]
         ]
+      },
+
+      {
+        uuid: '440e8400-e29b-41d4-a716-553311550025',
+        name: 'E Noé fez a arca em todos os aspectos, como Ele ordenou, no vigésimo sétimo jubileu do ano, na quinta semana, no quinto ano (na lua nova do primeiro mês).',
+        addYears: ['440e8400-e29b-41d4-a716-553311550000', this.jubileeCalc(27, 5, 5)],
+        source: [
+          ['JUB', 5, 22]
+        ]
+      },
+
+      {
+        uuid: '440e8400-e29b-41d4-a716-553311550026',
+        name: 'E ele entrou na sexta (ano) da mesma, no segundo mês, na lua nova do segundo mês, até o décimo sexto, e ele entrou e tudo o que lhe trouxe, na arca, e o Senhor fechou de fora, na noite XVII',
+        addYears: ['440e8400-e29b-41d4-a716-553311550000', this.jubileeCalc(27, 5, 6)],
+        source: [
+          ['JUB', 5, 22]
+        ]
+      },
+
+      {
+        uuid: '440e8400-e29b-41d4-a716-553311550027',
+        name: 'E as águas desapareceram acima da Terra na quinta semana em seu sétimo ano, e no décimo sétimo dia do segundo mês a terra estava seca.',
+        addYears: ['440e8400-e29b-41d4-a716-553311550000', this.jubileeCalc(27, 5, 7)],
+        source: [
+          ['JUB', 5, 30]
+        ]
+      },
+
+      {
+        uuid: '440e8400-e29b-41d4-a716-553311550028',
+        name: 'na sétima semana, no primeiro ano do mesmo, neste jubileu, Noé plantou vinha na montanha em que a arca descansou, chamado Lubar, uma das montanhas de Ararate.',
+        addYears: ['440e8400-e29b-41d4-a716-553311550000', this.jubileeCalc(27, 7, 1)],
+        source: [
+          ['JUB', 7, 1]
+        ]
+      },
+
+      {
+        uuid: '440e8400-e29b-41d4-a716-553311550029',
+        name: 'E no ano do jubileu vigésimo oitavo Noé começou a intimar a filhos de seus filhos os preceitos e mandamentos, e todos os julgamentos que ele sabia',
+        addYears: ['440e8400-e29b-41d4-a716-553311550000', this.jubileeCalc(28, 1, 1)],
+        source: [
+          ['JUB', 7, 20]
+        ]
       }
     ]
   };
+
+// [Capítulo 8]
+// 1 No vigésimo nono jubileu, na primeira semana, no início desta Arpachshad tomou para si uma
+// esposa e seu nome era Rasu'eja, a filha de Susan, a filha de Elam, e ela
+// 2 lhe deu um filho no terceiro ano, nesta semana, e ele chamou o seu nome Kainam. E o filho
+// cresceu, e seu pai lhe ensinou a escrever, e ele passou a procurar por si mesmo um lugar onde ele
+// poderia aproveitar para
+// 3-se de uma cidade. E ele encontrou um escrito que o ex-(gerações) tinha esculpido na rocha, e ele
+// leu o que estava nela, e ele transcreveu e pecou devido a ele, pois ele continha a doutrina da
+// Sentinelas em conformidade com o que eles usaram para observar o presságios do sol e da lua e
+// 4 estrelas em todos os sinais do céu. E ele escreveu e não disse nada a respeito dele, pois ele foi
+// 5 medo de falar sobre isso a Noé para que ele não deveria ficar bravo com ele por conta disso. E no
+// ano do jubileu trigésima, na segunda semana, no primeiro ano dele, ele tomou para si uma esposa, e
+// seu nome era Melka, a filha do Madai, filho de Jafé, e no quarto ano, que gerou um filho, e
+// 6 chamou o seu nome Selá; pois ele disse: 'Em verdade vos foram enviados. [E no quarto ano ele
+// nasceu], e Selá cresceu e tomou para si uma esposa, e seu nome era Mu'ak, filha de Kesed, irmão de
+// 13seu pai, em um trigésimo do jubileu e, na quinta semana , no primeiro ano
+// 7 º. E ela lhe deu um filho, no quinto ano do mesmo, e ele pôs o nome de Eber, e ele tomou para si
+// uma esposa, e seu nome era "Azurad, filha de Nebrod, no trigésimo segundo
+// 8 Jubileu, na sétima semana, no seu terceiro ano. E no seu sexto ano, ela deu-lhe o filho, e chamou
+// o seu nome Pelegue, pois nos dias em que ele nasceu os filhos de Noé começou
+// 9 de dividir a terra entre si: por isso chamou o seu nome Peleg. E eles
+// 10 dividido (isto) secretamente entre si, e disse que a Noé. E sucedeu que, no início do jubileu
+// trigésimo terceiro que divide a Terra em três partes, por Sem e Jafé e Cam, segundo a herança de
+// cada um, no primeiro ano na primeira semana, quando um de nós
+// 11 que tinham sido enviados, foi com eles. E chamou seus filhos, e se aproximaram a ele, eles e
+// seus filhos, e ele dividiu a terra em lotes, que seus três filhos estavam a tomar posse, e estendeu suas
+// mãos, e teve a redação de no seio de Noé, seu pai.
+// 12 E lá veio a escrever como muito Sem é o meio da terra que ele deveria ter como herança para si e
+// para seus filhos, para as gerações de eternidade, a partir do meio da serra de Rafa, a partir da foz do
+// água do rio Tina, e sua parcela vai para o oeste pelo meio deste rio, e se estende até que se chegue a
+// água dos abismos, dos quais o rio avança e despeja suas águas no mar Me'at, e este rio desagua no
+// mar grande. E tudo o que é para o norte é Jafé, e tudo o que é para o
+// 13 sul pertence Sem. E estende-se até alcançar Karaso: este é no seio da língua
+// 14, que olha para o sul. E a sua parte se estende ao longo do mar grande, e se estende em linha reta
+// até atingir o oeste da língua, que olha para o sul: por esse mar é
+// 15 nomeado a língua do mar do Egito. E acontece daqui para o sul em direção à foz do grande mar
+// na costa do (a) As águas, e se estende até o oeste para 'Afra, e estende-se até alcançar as águas do
+// rio Giom, e ao sul das águas do Gião, a
+// 16 margens deste rio. E isso se estende para o leste, até chegar ao Jardim do Éden, para o sul do
+// mesmo, [a] ao sul e do leste de toda a terra de Éden e de todo o leste, ele se vira para o leste e
+// prossegue até que atinge o leste da montanha chamado Rafa, e desce
+// 17 para o banco da boca do rio Tina. Esta parte saiu por sorteio para Sem e seus filhos,
+// 18 que deveriam possuir para sempre aos seus gerações para sempre. E Noé alegrou-se que esta
+// parcela saiu de Sem e para seus filhos, e ele se lembrou de tudo que tinha falado com a boca na
+// profecia, pois ele havia dito:
+// "Bendito seja o Senhor Deus de Sem
+// E que o Senhor habita na casa do Sem.
+// 19 E ele sabia que o Jardim do Éden é o santo dos santos, ea habitação do Senhor, e Mount Sinai no
+// centro do deserto, e Monte Sião, o centro do umbigo da terra, esses três
+// 20 foram criados como locais sagrados frente para o outro. E abençoou Deus dos deuses, que tinha
+// colocado o
+// 21 Palavra do Senhor em sua boca, e o Senhor para sempre. E ele sabia que uma parte abençoado e
+// uma bênção tinha chegado a Sem e seus filhos até as gerações para sempre, toda a terra do Éden e
+// toda a terra do mar Vermelho, e toda a terra do oriente e na Índia, e na Mar Vermelho e as
+// montanhas dos mesmos, e toda a terra de Basã, e todas as terras do Líbano e as ilhas de Kaftur, e
+// todas as montanhas de Sanir e 'Amana, e as montanhas da Assíria, no norte, e toda a terra do Elam,
+// Assíria, e Babel, e Susan e Ma'edai, e todas as montanhas de Ararate, e toda a região além do mar,
+// que é além das montanhas da Assíria, para o
+// 22 norte, uma terra abençoada e espaçoso, e tudo o que é muito bom. E para Ham saiu a segunda
+// parcela, além do Giom em direção ao sul, à direita do Jardim, e que se estende em direção ao sul e
+// que se estende a todas as montanhas de fogo, e se estende em direção ao oeste com o mar de «Atel e
+// se estende em direção ao oeste até atingir o mar de Ma'uk que (mar) em que
+// 23 tudo o que não é destruído desce. E ela sai em direção ao norte até os limites de Gadir, e sai para
+// a costa das águas do mar para as águas do grande mar até que se aproxima para o rio Giom, e vai ao
+// longo do rio até que Giom atinge o direito do Jardim
+// 1424 do Éden. E esta é a terra que saiu de Ham como a parte que ele estava a ocupar
+// 25 para sempre para si e seus filhos até suas gerações, para sempre. E para Jafé saíram da terceira
+// parte além do rio Tina ao norte da vazão de suas águas, e se estende ao norte-
+// 26 de leste a toda a região de Gog e de todos os países do leste da mesma. E isso se estende para
+// norte até ao norte, e se estende até as montanhas de Quelt em direção ao norte, e para o mar de
+// 27 Ma'uk, e sai para o leste de Gadir na medida em que a região das águas do mar. E isso se estende
+// até que se aproxima do oeste de Fara e retorna para "Aferag, e que se estende de leste
+// 28 para as águas do mar de Me'at. E isso se estende para a região do rio Tina, em direcção nordeste,
+// até que se aproxima do limite de suas águas para a montanha Rafa, e gira
+// 29 rodada para o norte. Esta é a terra que saiu de Jafé e seus filhos como parte de sua herança que
+// ele deve possuir para si e para seus filhos, para as suas gerações para sempre;
+// 30 cinco ilhas grandes, e uma grande terra no norte. Mas é fria, e as terras de Ham está quente, e as
+// terras de Sem é nem quente nem fria, que é misturado a frio e calor.
 
   genesis: TimelineData = {
     name: 'Gênesis',
